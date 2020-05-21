3c5ac892e395f6046dda232609ee3bf42cc7f937
DiinoAlmeida <61515055+DiinoAlmeida@users.noreply.github.com>
2020-05-21 15:53:24 +0100

    Add files via upload

--- QuadTree.md/QuadTree.c
+++ QuadTree.md/QuadTree.c
@@ -5,19 +5,18 @@ float Sx[4] = {-0.25, 0.25, -0.25, 0.25};
 float Sy[4] = {0.25, 0.25, -0.25, -0.25};
 
 struct NODE{
-
   struct NODE * Pos[4];
-  /*
-  struct NODE * NW;
-  struct NODE * NE;
-  struct NODE * SW;
-  struct NODE * SE;
-  */
   int x, y;
   int colour;
   //Cell Value;
 };
 
+/*
+typedef struct{
+  int x;
+  int y;
+}Point;
+*/
 
 typedef struct{
   struct NODE *root;
@@ -32,12 +31,6 @@ QuadTree make(){
 
 struct NODE * CreatePNode(int colour){
   struct NODE* new = (struct NODE *)malloc(sizeof(struct NODE));
-  /*
-  new->NW = NULL;
-  new->NE = NULL;
-  new->SW = NULL;
-  new->SE = NULL;
-  */
 
   for(int i = 0; i < 4; i++) new->Pos[i] = NULL;
   new->colour = colour;
@@ -47,36 +40,18 @@ struct NODE * CreatePNode(int colour){
 
 /*
 1 = GRAY (nonleaf node)
-2 = BLACK (contais a data point)
-3 = WHITE (is empty)
+2 = White (contais a data point / leaf)
 */
 
-//verifica se o nó é vazia e se não é folha
-/*int node_isEmpty(NODE* node){
-  return node->NW == NULL && node->NE == NULL && node->SW == NULL && node->SE == NULL && node->colour == 1;
-}*/
+
 /*
-static struct NODE* get_node(struct NODE* no, int q){
-  switch (q) {
-      case 1:
-       return no->NW;
-       break;
-      case 2:
-       return no->NE;
-       break;
-      case 3:
-       return no->SW;
-       break;
-      case 4:
-       return no->SE;
-       break;
+ * NW = 0
+ * NE = 1
+ * SW = 2
+ * SE = 3
+ */
 
-      default:
-        return NULL;
-       break;
-  }
-}
-*/
+
 int PRCompare(struct NODE *P, int X, int Y){
 
   if(P->x < X){
@@ -88,88 +63,61 @@ int PRCompare(struct NODE *P, int X, int Y){
   }else if(P->y < Y){
     return 3;
   }
-
+  
   return 1;
 }
 
-struct NODE * CopyMemory(struct NODE *no){
-
-  struct NODE * new = (struct NODE *)malloc(sizeof(struct NODE));
-  /*
-  new->NW = no->NW;
-  new->NE = no->NE;
-  new->SW = no->SW;
-  new->SE = no->SE;
-  */
-  new->x  = no->x;
-  
-  for(int i = 0; i < 4; i++) new->Pos[i] = no->Pos[i];
-  new->y = no->y;
-  new->colour = no->colour;
-
-  return new;
-}
-
-/*
- * NW = 0
- * NE = 1
- * SW = 2
- * SE = 3
- */
 void PRInsert(struct NODE *no, struct NODE **root, int X, int Y, int Lx, int Ly){
 
   struct NODE *T;
   struct NODE *U;
-  int q; //Quadrante
+  int q;
   int qu;
 
   if((*root) == NULL){ //Se não existir nenhum nó no root
-    (*root) = CopyMemory(no);
-    //printf("1(%d,%d)", (*root)->x, (*root)->y);
+    (*root) = no;
     return;
   }else if((*root)->colour != 1){ //Quando já temos um no 
     if((no->x == (*root)->x) && (no->y == (*root)->y)){ //Se o root for igual ao no terminamos a funcao
       return;
     }else{  //Se não for vemos em que quadrante é que o root vai ficar 
-      U = CopyMemory((*root));
+      U = (*root);
       (*root) = CreatePNode(1);
       q = PRCompare(U, X, Y);
-      printf("2(%d,%d) %d", U->x, U->y, q);
       (*root)->Pos[q] = U;
     }
   }
 
-  T = CopyMemory((*root));
+  T = (*root);
   q = PRCompare(no, X, Y);
 
-  while(T->Pos[q] != NULL && T->Pos[q]->colour == 1){ //AQUI O NOT NO PSEUDOCÓDIGO É PARA TODA A EXPRESSÃO OU SÓ PARA T null?
+  while(T->Pos[q] != NULL && T->Pos[q]->colour == 1){
     T = T->Pos[q];
-    X = X + Sx[q-1]*Lx;
+    X = X + Sx[q]*Lx;
     Lx = Lx/2;
-    Y = Y + Sy[q-1]*Ly;
+    Y = Y + Sy[q]*Ly;
     Ly = Ly/2;
     q = PRCompare(no,X,Y);
-    printf("\n\n\n\n%d\n\n\n", q);
   }
 
-  
-  if( T->Pos[q] == NULL) T->Pos[q] = CopyMemory(no);
+  if( T->Pos[q] == NULL){
+    T->Pos[q] = no;
+  }
   else if((no->x == T->Pos[q]->x) && (no->y == T->Pos[q]->y)) return; //ja foi inserido
   else{
-    U = CopyMemory(T->Pos[q]);
-    
+    U = T->Pos[q];
     do{
       T->Pos[q] = CreatePNode(1);
-      T = CopyMemory(T->Pos[q]);
-      X = X + Sx[q-1]*Lx;
+      T = T->Pos[q];
+      X = X + Sx[q]*Lx;
       Lx = Lx/2;
-      Y = Y + Sy[q-1]*Ly;
+      Y = Y + Sy[q]*Ly;
       Ly = Ly/2;
       q = PRCompare(no,X,Y);
       qu = PRCompare(U,X,Y);
     }while(q == qu);
 
-    T->Pos[q] = CopyMemory(no);
-    T->Pos[qu] = CopyMemory(U);
+    T->Pos[q] = no;
+    T->Pos[qu] = U;
   }
 }
--- QuadTree.md/main.c
+++ QuadTree.md/main.c
@@ -4,33 +4,44 @@
 int main(){
 
   QuadTree Q = make();
-  struct NODE *no;
+  struct NODE *no = CreatePNode(2);
+  struct NODE *p = CreatePNode(2);
+  struct NODE *no2 = CreatePNode(2);
+  struct NODE *no3 = CreatePNode(2);
   no->x = 10;
   no->y = 10;
-  no->colour = 2;
   PRInsert(no, &Q.root, 8, 8, 16, 16);
 
+  p->x = 2;
+  p->y = 2;
+  PRInsert(p, &Q.root, 8, 8, 16, 16);
+
+  no2->x = 11;
+  no2->y = 3;
+  PRInsert(no2, &Q.root, 8, 8, 16, 16);
+
+
+  no3->x = 15;
+  no3->y = 2;
+  PRInsert(no3, &Q.root, 8, 8, 16, 16);
   
-  no->x = 2;
-  no->y = 2;
-  no->colour = 2;
-  PRInsert(no, &Q.root, 8, 8, 16, 16);
-
   printf("\nroot: (%d,%d) %d\n", Q.root->x, Q.root->y, Q.root->colour);
-  if(Q.root->Pos[0] != NULL){
+  if(Q.root->Pos[3]->Pos[0] != NULL){
     printf("NW DIFERENT NULL\n");
   }
 
-  if(Q.root->Pos[1] != NULL){
+  if(Q.root->Pos[3]->Pos[1] != NULL){
     printf("NE DIFERENT NULL\n");
   }
 
-  if(Q.root->Pos[2] != NULL){
+  if(Q.root->Pos[3]->Pos[2] != NULL){
     printf("SW DIFERENT NULL\n");
   }
 
-  if(Q.root->Pos[3] != NULL){
+  if(Q.root->Pos[3]->Pos[3] != NULL){
     printf("SE DIFERENT NULL\n");
   }
+  
+  
   return 0;
 }
